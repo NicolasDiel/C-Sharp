5310266efe5fc6080b8c016aa64a5a39e3d97d87
NicolasDiel <71090440+NicolasDiel@users.noreply.github.com>
2021-07-07 22:22:40 -0300

    Add files via upload

---
+++ TinyRpgGame/Items/Equipment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
+public class Equipment : Item
+{
+    public EquipmentSlot equipSlot; // Slot to store in equipment in
+    public SkinnedMeshRenderer mesh;
+    public EquipmentMeshRegion[] coveredMeshRegions;
+
+    public int armorModifier;   // Increase/decrease in armor
+    public int damageModifier; // Increase/decrease in armor
+
+    //When pressed in inventory
+    public override void Use()
+    {
+        base.Use();
+        EquipmentManager.instance.Equip(this); // Equip it
+        RemoveFromInventory();  // Remove it from inventory
+    }
+}
+public enum EquipmentSlot { Head, Chest, Legs, Weapon, Shield, Feet}
+public enum EquipmentMeshRegion {Legs, Arms, Torso}; // Corresponds to body blendshapes
